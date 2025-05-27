167b246ec7eafc8b448a9ac2823388cadac91b56
Philip Veremeichik <p.veremeichik@richbrains.net>
2025-05-27 17:54:42 +0300

    add styles

--- src/widgets/form/index.tsx
+++ src/widgets/form/index.tsx
@@ -6,10 +6,10 @@ import { FormPayment } from "../../components/form-payment";
 
 export const FormPaymentWidget = observer(() => {
   return (
-    <div className="flex flex-col gap-[20px] bg-[#ECEBFF] py-[40px] px-[60px]">
+    <div className="flex flex-col gap-[16px] md:gap-[20px] bg-[#ECEBFF] py-[24px] md:py-[40px] px-[12px] md:px-[60px]">
       {paymentStore.step === "form" && (
         <div className="flex justify-left">
-          <h3 className="text-[38px] font-bold">Personal Details</h3>
+          <h3 className="text-[22px] md:text-[38px] font-bold">Personal Details</h3>
         </div>
       )}
       {paymentStore.step === "loading" && <Loader />}
